b62299c7b81e826e248817c413f6273a7bfcccb8
David Åse <david@wgtwo.com>
2018-02-13 20:22:30 +0100

    Improve mobile menu (add fade and enlarge clickable area)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 document.querySelector(".mobile-menu-trigger").addEventListener("click", function(e) {
     e.stopPropagation();
-    document.querySelector(".nav-menu").classList.toggle("open")
+    document.querySelector(".nav-menu").classList.toggle("open");
 });
 
 ["click", "touchend"].forEach(function(eventType) {
-    document.body.addEventListener(eventType, function() {
+    document.body.addEventListener(eventType, function(e) {
         document.querySelector(".nav-menu").classList.remove("open")
-    })
+    });
 });
