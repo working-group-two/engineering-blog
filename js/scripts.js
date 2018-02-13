2fc8e293a6dd12b7e1d30ab6ea90d4e165050abc
David Åse <david@wgtwo.com>
2018-02-13 19:13:18 +0100

    Add mobile menu (+ minor refactors)

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.querySelector(".mobile-menu-trigger").addEventListener("click", function(e) {
+    e.stopPropagation();
+    document.querySelector(".nav-menu").classList.toggle("open")
+});
+
+["click", "touchend"].forEach(function(eventType) {
+    document.body.addEventListener(eventType, function() {
+        document.querySelector(".nav-menu").classList.remove("open")
+    })
+});
