7ddad9fdcdfdb7d45144b230d1dd893a121140d2
David Åse <david@wgtwo.com>
2018-02-13 20:49:11 +0100

    Adjust mobile menu (position icon and overlay, remove fade again)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-document.querySelector(".mobile-menu-trigger").addEventListener("click", function(e) {
+document.querySelector(".mobile-menu-trigger").addEventListener("click", function (e) {
     e.stopPropagation();
     document.querySelector(".nav-menu").classList.toggle("open");
 });
 
-["click", "touchend"].forEach(function(eventType) {
-    document.body.addEventListener(eventType, function(e) {
-        document.querySelector(".nav-menu").classList.remove("open")
+["click", "touchend"].forEach(function (eventType) {
+    document.body.addEventListener(eventType, function (e) {
+        if (!e.target.classList.contains("nav-link")) {
+            document.querySelector(".nav-menu").classList.remove("open");
+        }
     });
 });
